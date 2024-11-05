144f49a05dfc64f80db9f918e6baa50cb237949d
alannguyen2003 <nguyenho30112003@gmail.com>
2024-11-05 16:49:53 +0700

    add version 2 for employees route

--- routes/employee-gateway.php
+++ routes/employee-gateway.php
@@ -1,12 +1,27 @@
 <?php
 
 const route = "EmployeeController@";
+const routeLink = "/employees";
 
-$router->group(['prefix' => '/api/v1/employees'], function () use ($router) {
-  //Employee routes
-  $router->get("/index", route."index");
-  $router->get("/find/{id}", route."find");
-  $router->post("/store", route."store");
-  $router->post("/update", route."update");
-  $router->delete("/delete", route."delete");
+$router->group(['prefix' => '/api'], function () use ($router) {
+  // Version 1
+  $router->group(['prefix' => '/v1'.routeLink], function () use ($router) {
+    //Employee routes
+    $router->get("/index", route."index");
+    $router->get("/find/{id}", route."find");
+    $router->post("/store", route."store");
+    $router->post("/update", route."update");
+    $router->delete("/delete", route."delete"); 
+  });
+
+  // Version 2
+  $router->group(['prefix' => '/v2'.routeLink], function () use ($router) {
+    //Employee routes
+    $router->get("/index", route."index");
+    $router->get("/find/{id}", route."find");
+    $router->post("/store", route."store");
+    $router->post("/update", route."update");
+    $router->delete("/delete", route."delete"); 
+  });
+  
 });
